eef1bb49b7db9d8ac75d284302139e238e8cf756
robertdlawrence1 <robertdlawrence1@gmail.com>
2025-03-31 22:43:18 -0500

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,11 @@
 const { onRequest } = require("firebase-functions/v2/https");
 const functions = require("firebase-functions");
 
-// Gen 2, region + secret declaration (omit serviceAccountEmail)
-exports.generateMetadataTest = onRequest({
+// Gen 2, region + secret declaration (using serviceAccountEmail explicitly)
+exports.generateMetadataV2 = onRequest({
   region: "us-central1",
-  secrets: ["CLAUDE_API_KEY"], // Claude secret already set in Firebase
+  secrets: ["CLAUDE_API_KEY"],
+  serviceAccountEmail: "digital-library-4f53e@appspot.gserviceaccount.com",
 }, async (req, res) => {
   try {
     return res.status(200).json({
